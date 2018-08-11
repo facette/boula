dcb192247696d53849284ebe17a9da3dd925923c
Vincent Batoufflet <vincent@batoufflet.info>
2018-08-11 12:47:56 +0200

    Add support to add/remove Y-Axis lines

--- src/components/lines.js
+++ src/components/lines.js
@@ -1,5 +1,22 @@
 export default function(Chart) {
     Chart.components.register({
+        init() {
+            Object.assign(this, {
+                addLine(id, line) {
+                    this.config.axes.y.lines.push(Object.assign(line, {id}));
+                    this.draw();
+                },
+
+                removeLine(id) {
+                    let length = this.config.axes.y.lines.length;
+                    this.config.axes.y.lines = this.config.axes.y.lines.filter(a => a.id !== id);
+                    if (this.config.axes.y.lines.length !== length) {
+                        this.draw();
+                    }
+                },
+            });
+        },
+
         afterDraw() {
             this.ctx.save();
             this.ctx.font = `${this.config.axes.y.ticks.font.size - 2}px ${this.config.font.family}`;
@@ -7,46 +24,62 @@ export default function(Chart) {
             this.ctx.textBaseline = "middle";
 
             this.config.axes.y.lines.forEach((line) => {
-                let pos = this.yScale(line.y);
+                let pos = this.yScale(line.y),
+                    xDelta = 0;
+
+                if (!line.color) {
+                    line.color = this.config.font.color;
+                }
 
                 if (line.label) {
                     let text = typeof line.label == "boolean" ? this.yFormat(line.y) : line.label,
                         measure = this.ctx.measureText(text);
 
-                    let delta = 6,
-                        deltaLow = Math.round(delta / 3),
-                        deltaMedium = Math.round(delta * 2 / 3),
+                    let yDelta = 6,
+                        yDeltaLow = Math.round(yDelta / 3),
+                        yDeltaMedium = Math.round(yDelta * 2 / 3),
                         yStart = -this.config.axes.x.ticks.size - measure.width;
 
-                    this.ctx.fillStyle = line.color || this.config.font.color;
+                    this.ctx.save();
+
+                    xDelta = Math.abs(yStart) - this.area.left + this.config.margin / 2;
+                    if (xDelta > 0) {
+                        this.ctx.translate(xDelta, 0);
+                    }
+
+                    this.ctx.fillStyle = line.color;
 
                     // Draw label background
                     this.ctx.beginPath();
-                    this.ctx.moveTo(yStart, pos - delta);
-                    this.ctx.lineTo(-this.config.axes.x.ticks.size, pos - delta);
-                    this.ctx.quadraticCurveTo(-deltaMedium, pos - delta, 0, pos);
-                    this.ctx.quadraticCurveTo(-deltaMedium, pos + delta, -this.config.axes.x.ticks.size, pos + delta);
-                    this.ctx.lineTo(yStart, pos + delta);
-                    this.ctx.quadraticCurveTo(yStart - deltaLow, pos + delta, yStart - deltaLow, pos + deltaMedium);
-                    this.ctx.lineTo(yStart - deltaLow, pos - deltaMedium);
-                    this.ctx.quadraticCurveTo(yStart - deltaLow, pos - delta, yStart, pos - delta);
+                    this.ctx.moveTo(yStart, pos - yDelta);
+                    this.ctx.lineTo(-this.config.axes.x.ticks.size, pos - yDelta);
+                    this.ctx.quadraticCurveTo(-yDeltaMedium, pos - yDelta, 0, pos);
+                    this.ctx.quadraticCurveTo(-yDeltaMedium, pos + yDelta, -this.config.axes.x.ticks.size,
+                        pos + yDelta);
+                    this.ctx.lineTo(yStart, pos + yDelta);
+                    this.ctx.quadraticCurveTo(yStart - yDeltaLow, pos + yDelta, yStart - yDeltaLow,
+                        pos + yDeltaMedium);
+                    this.ctx.lineTo(yStart - yDeltaLow, pos - yDeltaMedium);
+                    this.ctx.quadraticCurveTo(yStart - yDeltaLow, pos - yDelta, yStart, pos - yDelta);
                     this.ctx.closePath();
                     this.ctx.fill();
 
                     // Draw text on label
                     this.ctx.fillStyle = Chart.helpers.toRGBA(this.config.background.color, 0.8);
                     this.ctx.fillText(text, -this.config.axes.x.ticks.size, pos);
+
+                    this.ctx.restore();
                 }
 
                 // Draw line
                 this.ctx.lineWidth = 1;
-                this.ctx.strokeStyle = line.color || Chart.helpers.toRGBA(this.config.font.color, 0.1);
+                this.ctx.strokeStyle = line.color;
 
                 this.ctx.beginPath();
                 if (line.dashed) {
                     this.ctx.setLineDash([4, 4]);
                 }
-                this.ctx.moveTo(0, pos);
+                this.ctx.moveTo(xDelta > 0 ? xDelta : 0, pos);
                 this.ctx.lineTo(this.area.width, pos);
                 this.ctx.stroke();
             });
